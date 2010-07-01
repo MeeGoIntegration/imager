29b46c40bab2c1f1f120feaa51cf1effb41e7dae
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2010-07-01 15:47:38 +0300

    IMG settings

---
+++ src/meego_img/imgsettings.py
@@ -0,0 +1,7 @@
+base_url = "http://212.149.247.53:8001/images/"
+base_dir = "/var/www/images"
+amqp_host = "localhost:5672"
+amqp_user = "img"
+amqp_pwd = "imgpwd"
+amqp_vhost = "imgvhost"
+num_workers = 2

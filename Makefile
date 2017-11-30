ee77e6dad2a762bf980cf0eb6bdbb081c9444fca
David Greaves <david@dgreaves.com>
2017-11-30 20:08:29 +0000

    Add the make_vdi participant used to create VirtualBox images for the SDK
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- Makefile
+++ Makefile
@@ -9,18 +9,21 @@ install:
 	python setup.py -q install --root=$(DESTDIR) --prefix=$(PREFIX)
 	install -D -m 755 src/img_boss/build_image.py         $(DESTDIR)/usr/share/boss-skynet/build_image.py
 	install -D -m 755 src/img_boss/build_ks.py            $(DESTDIR)/usr/share/boss-skynet/build_ks.py
+	install -D -m 755 src/img_boss/make_vdi.py            $(DESTDIR)/usr/share/boss-skynet/make_vdi.py
 	install -D -m 755 src/img_boss/test_vm_image.py       $(DESTDIR)/usr/share/boss-skynet/test_vm_image.py
 	install -D -m 755 src/scripts/img_test_vm.sh          $(DESTDIR)/usr/bin/img_test_vm.sh
 	install -D -m 755 src/scripts/img_host_test.sh        $(DESTDIR)/usr/bin/img_host_test.sh
 	install -D -m 755 src/scripts/img_vm_shutdown         $(DESTDIR)/usr/bin/img_vm_shutdown
 	install -D -m 644 src/img_boss/build_image.conf       $(DESTDIR)/etc/skynet/build_image.conf
 	install -D -m 644 src/img_boss/build_ks.conf          $(DESTDIR)/etc/skynet/build_ks.conf
+	install -D -m 644 src/img_boss/make_vdi.conf          $(DESTDIR)/etc/skynet/make_vdi.conf
 	install -D -m 644 src/img_boss/test_vm_image.conf     $(DESTDIR)/etc/skynet/test_vm_image.conf
 	install -D -m 755 src/img_boss/update_image_status.py $(DESTDIR)/usr/share/boss-skynet/update_image_status.py
 	install -D -m 755 src/img_boss/request_image.py       $(DESTDIR)/usr/share/boss-skynet/request_image.py
 	install -D -m 644 src/img_boss/request_image.conf     $(DESTDIR)/etc/skynet/request_image.conf
 	install -D -m 644 conf/supervisor/request_image.conf  $(DESTDIR)/etc/supervisor/conf.d/request_image.conf
 	install -D -m 644 conf/supervisor/build_ks.conf       $(DESTDIR)/etc/supervisor/conf.d/build_ks.conf
+	install -D -m 644 conf/supervisor/make_vdi.conf       $(DESTDIR)/etc/supervisor/conf.d/make_vdi.conf
 	install -D -m 644 conf/supervisor/test_vm_image.conf  $(DESTDIR)/etc/supervisor/conf.d/test_vm_image.conf
 	install -D -m 644 conf/supervisor/build_image.conf    $(DESTDIR)/etc/supervisor/conf.d/build_image.conf
 	install -D -m 644 conf/supervisor/update_image_status.conf $(DESTDIR)/etc/supervisor/conf.d/update_image_status.conf
---
+++ src/img_boss/make_vdi.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+#~ Copyright (C) 2013 David Greaves <david@dgreaves.com>
+#~ This program is free software: you can redistribute it and/or modify
+#~ it under the terms of the GNU General Public License as published by
+#~ the Free Software Foundation, either version 3 of the License, or
+#~ (at your option) any later version.
+
+#~ This program is distributed in the hope that it will be useful,
+#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
+#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#~ GNU General Public License for more details.
+
+#~ You should have received a copy of the GNU General Public License
+#~ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Converts mic raw image to VDI format.
+
+.. warning ::
+
+   * The make_vdi participant must run on the master nfs share
+   * Must have run after build_image and relies on a valid
+     build_image workitem
+
+:term:`Workitem` fields IN:
+
+:Parameters:
+
+   :vboxmanage.process (Boolean):
+      If false then the participant is skipped
+   :vboxmanage.format (string):
+      As per VBoxManage --format; one of VDI|VMDK|VHD
+   :vboxmanage.variant (string):
+      OPTIONAL As per VBoxManage --variant; one or more of:
+        Standard,Fixed,Split2G,Stream,ESX
+
+:term:`Workitem` fields OUT:
+
+:Returns:
+  :result (Boolean):
+     True if everything was OK, False otherwise
+"""
+
+from RuoteAMQP.workitem import DictAttrProxy as dap
+from RuoteAMQP import Launcher 
+
+import os.path
+import subprocess
+import shutil
+
+class ParticipantHandler(object):
+    """Participant class as defined by the SkyNET API"""
+
+    def __init__(self):
+        self.worker_config = None
+
+    def handle_wi_control(self, ctrl):
+        """ job control thread """
+        pass
+
+    def handle_lifecycle_control(self, ctrl):
+        """ participant control thread """
+        if ctrl.message == "start":
+            if ctrl.config.has_section("make_vdi"):
+                self.config = dap({})
+                self.config.base_url = ctrl.config.get("make_vdi", "base_url")
+                self.config.base_dir = ctrl.config.get("make_vdi", "base_dir")
+                print "config base_url %s" % self.config.base_url
+
+    def handle_wi(self, wid):
+        """Handle the workitem to convert an image
+        """
+        f = wid.fields
+
+        if not f.image or not f.image.image_type or not f.image.image_url:
+            raise RuntimeError("Missing mandatory field 'image'")
+
+        if not f.image.image_type == "raw":
+            return False, "Image type is not raw"
+
+        raw = os.path.abspath(f.image.image_url.replace(self.config.base_url,
+                                                        self.config.base_dir))
+        if not os.path.isfile(raw):
+            return False, "Image file %s not present at %s\n" % (
+                f.image.image_url, raw)
+
+        if not raw.endswith(".raw"):
+            try:
+                orig=raw
+                # Try to uncompress
+                if raw.endswith(".gz"):
+                    print "gunzip %s\n" % raw
+                    subprocess.check_output(["gunzip", raw])
+                    raw = raw[0:-3]
+                if raw.endswith(".tar.bz2"):
+                    print "tar xv -C %s -f %s\n" % (os.path.dirname(raw), raw)
+                    new_raw = subprocess.check_output(["tar", "xv",
+                                                       "-C", os.path.dirname(raw),
+                                                       "-f", raw]
+                                                       ).rstrip('\n')
+                    subprocess.check_output(["rm", raw])
+                    raw = raw[0:-8] + ".raw"
+                    os.rename(os.path.join(os.path.dirname(raw), new_raw),
+                              raw)
+#                    new_raw = subprocess.check_output(["tar", "xv",
+#                                                       "-C", "/tmp",
+#                                                       "-f", raw]
+#                                                       ).rstrip('\n')
+#                    os.unlink(raw)
+#                    raw = raw[0:-8] + ".raw"
+#                    shutil.copy(os.path.join("/tmp", new_raw),
+#                                raw)
+#                    os.unlink(os.path.join("/tmp", new_raw))
+
+                if raw.endswith(".bz2"):
+                    print "bunzip2 %s\n" % raw                    
+                    subprocess.check_output(["bunzip2", raw])
+                    raw = raw[0:-4]
+            except subprocess.CalledProcessError,e:
+                return False, "Failed whilst trying to uncompress %s.\n%s\n" % (raw, e.output)
+            if not raw.endswith(".raw") and not os.path.isfile(raw):
+                return False, "Failed to convert %s to a .raw called %s" % (orig, raw)
+            print "Got %s" % raw
+        try:
+            vdi = raw[0:-3]+"VDI"
+            command = [ "VBoxManage", "convertfromraw",
+                        raw, vdi]
+            if f.vboxmanage and f.vboxmanage.format:
+                command.append("--format=%s" % f.vboxmanage.format)
+            else:
+                command.append("--format=%s" % "VDI")
+
+            if f.vboxmanage and f.vboxmanage.variant:
+                command.append("--variant=%s" % f.vboxmanage.variant)
+
+            print " ".join(command)
+            subprocess.check_output(command)
+            subprocess.check_call(["chmod", "0664", vdi])
+            subprocess.check_call(["bzip2", "--fast", vdi])
+            subprocess.check_call(["chown", "-R", "img", os.path.dirname(raw)])
+        except subprocess.CalledProcessError,e:
+            return False, "Running convertfromraw failed:\n%s" % e.output
+
+        # Cleanup
+        os.remove(raw)
+
+        return True

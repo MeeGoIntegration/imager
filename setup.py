96eb2c0a2865cbd406fb98d98cd715f7d3573b9e
David Greaves <david.greaves@jolla.com>
2020-04-07 10:04:05 +0100

    Cleanup python installation
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- setup.py
+++ setup.py
@@ -1,36 +1,29 @@
 # -*- coding: utf-8 -*-
-import os, sys
 from distutils.core import setup
 
 from setuptools import find_packages
 
-static_files=[('/etc/imager', ['src/img_web/img.conf']),
-                ('/usr/share/img_web/processes', 
-                   ['src/img_web/processes/CREATE_IMAGE',
-                   ]
-                )
-             ]
+static_files = [('/etc/imager', ['src/img_web/img.conf']),
+                ('/usr/share/img_web/processes',
+                 ['src/img_web/processes/CREATE_IMAGE'])]
 
 toplevel = 'src/'
 setup(
-    name = "img",
-    version = "0.6.0",
-    url = 'http://meego.gitorious.org/meego-infrastructure-tools/imger',
-    license = 'GPLv2',
-    description = "Meego Image creation service",
-    author = 'Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>',
-    packages = find_packages(toplevel),
-    package_dir = {'': toplevel},
-    package_data = { 'img_web' : ['templates/*.html',
-                                  'templates/app/*.html', 
-                                  'templates/registration/*.html',
-                                  ],
-                      'img_web.app' : ['static/*.*',
-                                       'static/images/*.*',
-                                       'static/images/formset/*.*',
-                                       'static/js/*.*',
-                                       'fixtures/*.*'
-                                      ]
-                    },
-    data_files = static_files,
+    name="img",
+    version="0.69.0",
+    url='https://github.com/MeegoIntegration/imager.git',
+    license='GPLv2+',
+    description="Meego Image creation service",
+    author='Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>',
+    packages=find_packages(toplevel),
+    package_dir={'': toplevel},
+    package_data={'img_web': ['templates/*.html',
+                              'templates/app/*.html',
+                              'templates/registration/*.html'],
+                  'img_web.app': ['static/*.*',
+                                  'static/images/*.*',
+                                  'static/images/formset/*.*',
+                                  'static/js/*.*',
+                                  'fixtures/*.*']},
+    data_files=static_files,
 )

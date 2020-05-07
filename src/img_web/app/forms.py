f3217d49e2506525802510be4dc9c05b261ba5eb
David Greaves <david.greaves@jolla.com>
2020-05-07 15:11:05 +0100

    lintian cleanup
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- src/img_web/app/forms.py
+++ src/img_web/app/forms.py
@@ -1,21 +1,23 @@
-#~ Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
-#~ Contact: Ramez Hanna <ramez.hanna@nokia.com>
-#~ This program is free software: you can redistribute it and/or modify
-#~ it under the terms of the GNU General Public License as published by
-#~ the Free Software Foundation, either version 3 of the License, or
-#~ (at your option) any later version.
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+# Contact: Ramez Hanna <ramez.hanna@nokia.com>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
-#~ This program is distributed in the hope that it will be useful,
-#~ but WITHOUT ANY WARRANTY; without even the implied warranty of
-#~ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#~ GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#~ You should have received a copy of the GNU General Public License
-#~ along with this program.  If not, see <http://www.gnu.org/licenforms.pyses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenforms.pyses/>.
 
 """ Image job creation forms """
 
-import os,re, glob
+import os
+import re
+import glob
 from itertools import chain
 from collections import defaultdict
 import ConfigParser
@@ -29,11 +31,13 @@ from django.utils.encoding import force_unicode, smart_unicode, smart_str
 
 from django.utils.html import escape, conditional_escape
 
+
 def get_features():
     config = ConfigParser.ConfigParser()
     for feature in glob.glob(os.path.join(settings.FEATURESDIR, '*.feature')):
         config.read(feature)
     return config
+
 
 def list_features():
     features = get_features()
@@ -47,9 +51,11 @@ def list_features():
         choices.add((name, description))
     return sorted(choices, key=lambda c: c[1])
 
+
 def expand_feature(name):
     features = get_features()
-    repo_sections = [ section for section in features.sections() if section.startswith("repositories") ]
+    repo_sections = [section for section in features.sections() if
+                     section.startswith("repositories")]
     feat = defaultdict(set)
 
     if features.has_option(name, "pattern"):
@@ -65,52 +71,64 @@ def expand_feature(name):
                     feat[section].add(features.get(section, repo))
     return dict(feat)
 
+
 class extraReposForm(forms.Form):
 
     obs = forms.ChoiceField(label="OBS", choices=[("None", "None")],
-                            help_text="Extra OBS instances from which packages"\
-                                      " may be downloaded from.")
-    project = forms.CharField(label = "Project", required=False, max_length=500,
-                              help_text = "Project name in which the repository "\
-                              "lives. For example: home:user")
-    repo = forms.CharField(label = "Repository", required=False, max_length=500,
-                           help_text = "Repository name in which the packages "\
-                           "live. For example: latest_i486")
+                            help_text="Extra OBS instances from which "
+                            "packages may be downloaded from.")
+    project = forms.CharField(label="Project", required=False,
+                              max_length=500,
+                              help_text="Project name in which"
+                              " the repository lives. For example: home:user")
+    repo = forms.CharField(label="Repository", required=False,
+                           max_length=500, help_text="Repository "
+                           "name in which the packages live. For "
+                           "example: latest_i486")
 
     def __init__(self, *args, **kwargs):
         super(extraReposForm, self).__init__(*args, **kwargs)
-        self.fields['obs'].choices = [("None", "None")] + [(obs.apiurl , obs.name) for obs in BuildService.objects.all()]
+        self.fields['obs'].choices = [("None", "None")] + \
+            [(obs.apiurl, obs.name) for obs in BuildService.objects.all()]
 
     def clean(self):
         cleaned_data = self.cleaned_data
-        if not 'obs' in cleaned_data or cleaned_data['obs'] == "None":
+        if 'obs' not in cleaned_data or cleaned_data['obs'] == "None":
             cleaned_data['obs'] = None
 
-        if not 'repo' in cleaned_data or cleaned_data['repo'] == "":
+        if 'repo' not in cleaned_data or cleaned_data['repo'] == "":
             cleaned_data['repo'] = None
         else:
             cleaned_data['repo'] = cleaned_data['repo'].strip()
 
-        if not 'project' in cleaned_data:
+        if 'project' not in cleaned_data:
             cleaned_data['project'] = ""
         else:
             cleaned_data['project'] = cleaned_data['project'].strip()
 
         if cleaned_data['obs'] and not cleaned_data['repo']:
-            raise forms.ValidationError("You chose an extra OBS without "\
+            raise forms.ValidationError("You chose an extra OBS without "
                                         "adding a corresponding repository.")
         return cleaned_data
 
+
 extraReposFormset = formset_factory(extraReposForm)
+
 
 class extraTokensForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(extraTokensForm, self).__init__(*args, **kwargs)
         for token in Token.objects.all():
-            self.fields[token.name] = forms.CharField(label=token.name, initial=token.default, required=False, help_text=token.description)
+            self.fields[token.name] = \
+                forms.CharField(label=token.name,
+                                initial=token.default,
+                                required=False,
+                                help_text=token.description)
+
 
 extraTokensFormset = formset_factory(extraTokensForm)
+
 
 class PostProcessForm(forms.Form):
 
@@ -121,9 +139,14 @@ class PostProcessForm(forms.Form):
             del(kwargs["pp"])
         super(PostProcessForm, self).__init__(*args, **kwargs)
         if pp:
-            self.fields[pp.name] = forms.BooleanField(label=pp.name, initial=pp.default, required=False, help_text=pp.description)
+            self.fields[pp.name] = \
+                forms.BooleanField(label=pp.name, initial=pp.default,
+                                   required=False, help_text=pp.description)
             if pp.argname:
-                self.fields[pp.argname] = forms.CharField(label=pp.argname, required=False, widget=forms.Textarea(attrs={'rows':'1'}), help_text=pp.description)
+                self.fields[pp.argname] = \
+                    forms.CharField(label=pp.argname, required=False,
+                                    widget=forms.Textarea(attrs={'rows': '1'}),
+                                    help_text=pp.description)
 
 
 class BasePostProcessFormset(BaseFormSet):
@@ -136,6 +159,7 @@ class BasePostProcessFormset(BaseFormSet):
             return self.form_kwargs[index]
         except LookupError:
             return {}
+
 
 class OptionAttrChoiceField(forms.ChoiceField):
 
@@ -161,7 +185,6 @@ class OptionAttrSelect(forms.Select):
     # This is the mainly standard method called per-option to be
     # passed to the template value can now be a value or a tuple/list
     # of (value, dict_of_attrs)
-    # Django 1.11
     def create_option(self, name, value, label, selected, index,
                       subindex=None, attrs=None):
         index = str(index) if subindex is None else "%s_%s" % (index, subindex)
@@ -194,69 +217,71 @@ class OptionAttrSelect(forms.Select):
 
 class ImageJobForm(forms.Form):
     """ Django form that allows users to create image jobs """
-    imagetype = forms.ChoiceField(label='Image type',
-                                  choices=[],
-                                  help_text="Type: format of image you want to"\
-                                            " produce.")
+    imagetype = forms.ChoiceField(
+        label='Image type',
+        choices=[],
+        help_text="Type: format of image you want to produce.")
 
-    architecture = forms.ChoiceField(label='Architecture',
-                                     choices=[],
-                                     help_text="Target architecture of the "\
-                                               "image you want to build from "\
-                                               "your customized kickstart.")
-    ksfile = forms.FileField(label="Kickstart file", required=False,
-                             help_text="Kickstart: customized kickstart file, "\
-                                       "if the templates don't fit your needs.")
+    architecture = forms.ChoiceField(
+        label='Architecture',
+        choices=[],
+        help_text="Target architecture of the image you want to build from "
+        "your customized kickstart.")
 
-    template = OptionAttrChoiceField(label='Template',
-                                 choices=[("None", "None")],
-                                 widget=OptionAttrSelect,
-                                help_text="Template: Choose a base template "\
-                                          "ontop of which your packages will "\
-                                          "be added. Each template is targeted"\
-                                          " at a certain device and "\
-                                          "architecture so the architecture "\
-                                          "and kickstart fields will be "\
-                                          "ignored.")
+    ksfile = forms.FileField(
+        label="Kickstart file", required=False,
+        help_text="Kickstart: customized kickstart file, "
+        "if the templates don't fit your needs.")
 
+    template = OptionAttrChoiceField(
+        label='Template',
+        choices=[("None", "None")],
+        widget=OptionAttrSelect,
+        help_text="Template: Choose a base template ontop of which your "
+        "packages will be added. Each template is targeted at a certain "
+        "device and architecture so the architecture and kickstart "
+        "fields will be ignored.")
 
-    features = forms.TypedMultipleChoiceField(label="Features", choices=[],
-                            help_text="Features: Commonly used extra features", empty_value={},
-                            coerce=expand_feature, required = False,
-                            widget=forms.widgets.CheckboxSelectMultiple)
+    features = forms.TypedMultipleChoiceField(
+        label="Features", choices=[],
+        help_text="Features: Commonly used extra features",
+        empty_value={},
+        coerce=expand_feature, required=False,
+        widget=forms.widgets.CheckboxSelectMultiple)
 
-    overlay = forms.CharField(label="Packages", required=False,
-                              widget=forms.Textarea(attrs={'rows':'1'}),
-                                                    help_text=\
-                              "Packages: comma separated list of packages you "\
-                              "want to include in the image built from the "\
-                              "chosen template. A packagename prefixed wtit "\
-                              '"-" is excluded. Package groups are denoted by '\
-                              '"@" prefix.')
-    pinned = forms.BooleanField(label="Pin image", required=False,
-                                initial=False,
-                            help_text="Pin image so it doesn't expire or get "\
-                                      "deleted by mistake. ")
-    tags = forms.CharField(label="Tags", required=False,
-                           widget=forms.Textarea(attrs={'rows':'1'}),
-                                                 help_text=\
-                              "Packages: comma separated list of tags "\
-                              "to describe the image built.")
+    overlay = forms.CharField(
+        label="Packages", required=False,
+        widget=forms.Textarea(attrs={'rows': '1'}),
+        help_text="Packages: comma separated list of packages you want to "
+        "include in the image built from the chosen template. A packagename "
+        "prefixed with '-' are excluded. Package groups are denoted by "
+        "'@' prefix.")
+
+    pinned = forms.BooleanField(
+        label="Pin image", required=False,
+        initial=False,
+        help_text="Pin image so it doesn't expire or get deleted by mistake.")
+
+    tags = forms.CharField(
+        label="Tags", required=False,
+        widget=forms.Textarea(attrs={'rows': '1'}),
+        help_text="Packages: comma separated list of tags to describe the "
+        "image built.")
+
     devicemodel = forms.CharField(
         label="Device model",
         required=False,
-        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}),
-    )
+        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}))
+
     devicevariant = forms.CharField(
         label="Device variant",
         required=False,
-        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}),
-    )
+        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}))
+
     brand = forms.CharField(
         label="Brand",
         required=False,
-        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}),
-    )
+        widget=forms.HiddenInput(attrs={'readonly': 'readonly'}))
 
     def __init__(self, *args, **kwargs):
         super(ImageJobForm, self).__init__(*args, **kwargs)
@@ -267,10 +292,11 @@ class ImageJobForm(forms.Form):
         device_re = re.compile(
             r'^#.*?(DeviceModel|DeviceVariant|Brand):(.*)$'
         )
-         # find the available ks files and extract the feature
-         # defaults from each file
-         for template in glob.glob(os.path.join(settings.TEMPLATESDIR,
-                                                '*.ks')):
+
+        # find the available ks files and extract the feature
+        # defaults from each file
+        for template in glob.glob(os.path.join(settings.TEMPLATESDIR,
+                                               '*.ks')):
             name = os.path.basename(template)
             templatename = os.path.basename(template)
             attrs = {}
@@ -291,8 +317,10 @@ class ImageJobForm(forms.Form):
         self.fields['template'].choices = sorted(choices, key=lambda
                                                  name: name[1])
         self.fields['template'].choices.insert(0, ("None", "None"))
-        self.fields['architecture'].choices = [(arch.name, arch.name) for arch in Arch.objects.all()]
-        self.fields['imagetype'].choices = [(itype.name, itype.name) for itype in ImageType.objects.all()]
+        self.fields['architecture'].choices = [
+            (arch.name, arch.name) for arch in Arch.objects.all()]
+        self.fields['imagetype'].choices = [
+            (itype.name, itype.name) for itype in ImageType.objects.all()]
         self.fields['features'].choices = list_features()
 
     def clean(self):
@@ -300,20 +328,24 @@ class ImageJobForm(forms.Form):
         if cleaned_data['template'] == "None":
             cleaned_data['template'] = None
 
-        if (('ksfile' in cleaned_data and 'template' in cleaned_data) and
+        if (('ksfile' in cleaned_data and 'template' in cleaned_data)
+            and
             (cleaned_data['ksfile'] and cleaned_data['template'])):
-            raise forms.ValidationError("Please choose template or upload"\
-                                            " a kickstart, not both!")
-        elif (('ksfile' not in cleaned_data and 'template' not in cleaned_data) and
+            raise forms.ValidationError("Please choose template or upload"
+                                        " a kickstart, not both!")
+        elif (('ksfile' not in cleaned_data and 'template' not in cleaned_data)
+              and
               (cleaned_data['ksfile'] and cleaned_data['template'])):
-            raise forms.ValidationError("Please choose either a template or"\
-                                            "upload a kickstart file.")
+            raise forms.ValidationError("Please choose either a template or"
+                                        "upload a kickstart file.")
         return cleaned_data
+
 
 class TagForm(forms.Form):
     tags = TagField()
 
-class SearchForm(forms.Form):
-    searchterm = forms.CharField(label="Search term", required=True,
-                                 help_text="partial or full tag name to search with")
 
+class SearchForm(forms.Form):
+    searchterm = forms.CharField(
+        label="Search term", required=True,
+        help_text="partial or full tag name to search with")
